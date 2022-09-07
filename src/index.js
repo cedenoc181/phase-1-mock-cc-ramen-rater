f58ee0fa75332c32f26396c412a07fe211d9adc6
Christian <cedenoc181@gmail.com>
2022-09-07 11:30:51 -0400

    mock cc submission

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
 // write your code here
+const ramenMenu = {
+shoyu:{
+    ramen: "Shoyu Ramen",
+    restaurant: "Nonono",
+    img: './assets/ramen/shoyu.jpg',
+    rating: 7,
+    comment: "Delish. Can't go wrong with a classic!"
+},
+gyukotsu:{
+    ramen: "Gyukotsu Ramen",
+    resturant: "Za-Ya Ramen",
+    img: './assets/ramen/gyukotsu.jpg',
+    rating: 8,
+    comment:"Good to the last drop."
+},
+kojiro: {
+    ramen: "Kojiro Red Ramen",
+    restaurant: "Ramen-Ya",
+    img: './assets/ramen/kojiro.jpg',
+    rating: 6,
+    comment: "Perfect for a cold night."
+},
+nirvana:{
+    ramen: "Nirvana Shiromaru",
+    restaurant: "Ippudo",
+    img: "./assets/ramen/nirvana.jpg",
+    rating: 7,
+    comment: "Do buy the hype."
+},
+naruto:{
+    ramen: "Naruto Ramen",
+    restaurant: "Naruto",
+    img: "./assets/ramen/naruto.jpg",
+    rating: 10,
+    comment: "My absolute fave!"
+},
+
+}
+
+//const ramenMenuEvent = document.getElementById('ramen-menu');
+document.addEventListener('DOMcontentLoaded',function (){
+  document.querySelector('#ramen-menu').append.GET(/ramens/);
+}
+)
+document.addEventListener('click', function (){
+    if('click'=== document.querySelector('#gyukotsu')){
+    GET(/ramen/1);
+    }
+})
+
+
+)
+ document.getElementById('ramen-menu').addEventListener("click", function (){
+
+  return document.getElementById('detail-image').append(document.getElementById("shoyu"))
+
+ });  
